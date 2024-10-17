cd776753ce8b8758969aadf81e00f63d981e2dbe
bhanusunrise <pasindubhanukagood@gmail.com>
2024-10-18 02:41:41 +0530

    Fully migrated

---
+++ prisma/migrations/20241017210852_complete_client_migration/migration.sql
@@ -0,0 +1,145 @@
+-- CreateTable
+CREATE TABLE `Units` (
+    `unit_id` VARCHAR(7) NOT NULL,
+    `unit_name` VARCHAR(50) NOT NULL,
+    `abbreviation` VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY (`unit_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Unit_Conversions` (
+    `conversion_id` VARCHAR(8) NOT NULL,
+    `from_unit` VARCHAR(10) NOT NULL,
+    `value` DOUBLE NOT NULL,
+    `to_unit` VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY (`conversion_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Items` (
+    `item_id` VARCHAR(10) NOT NULL,
+    `item_name` VARCHAR(50) NOT NULL,
+    `item_category_id` VARCHAR(6) NOT NULL,
+
+    INDEX `Items_item_category_id_idx`(`item_category_id`),
+    PRIMARY KEY (`item_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Item_Categories` (
+    `category_id` VARCHAR(6) NOT NULL,
+    `category_name` VARCHAR(30) NOT NULL,
+
+    PRIMARY KEY (`category_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ItemUnits` (
+    `id` VARCHAR(6) NOT NULL,
+    `item_id` VARCHAR(10) NOT NULL,
+    `unit_id` VARCHAR(7) NOT NULL,
+
+    UNIQUE INDEX `ItemUnits_item_id_unit_id_key`(`item_id`, `unit_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Stocks` (
+    `stock_id` VARCHAR(10) NOT NULL,
+    `total_quantity` INTEGER NOT NULL,
+    `damaged_quantity` INTEGER NOT NULL,
+    `sold_quantity` INTEGER NOT NULL,
+    `item_id` VARCHAR(10) NOT NULL,
+    `buying_date` DATE NOT NULL,
+    `buying_price` DOUBLE NOT NULL,
+    `selling_price` DOUBLE NOT NULL,
+    `barcode_url` VARCHAR(20) NOT NULL,
+    `supplier_id` VARCHAR(8) NOT NULL,
+
+    INDEX `Stocks_item_id_idx`(`item_id`),
+    INDEX `Stocks_supplier_id_idx`(`supplier_id`),
+    PRIMARY KEY (`stock_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Suppliers` (
+    `supplier_id` VARCHAR(8) NOT NULL,
+    `supplier_name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `phone` INTEGER NOT NULL,
+    `address` VARCHAR(200) NOT NULL,
+
+    INDEX `Suppliers_supplier_id_idx`(`supplier_id`),
+    PRIMARY KEY (`supplier_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Bills` (
+    `bill_id` VARCHAR(10) NOT NULL,
+    `bill_date` DATE NOT NULL,
+    `barcode_url` VARCHAR(20) NOT NULL,
+    `sub_total_amount` DOUBLE NOT NULL,
+    `discount_amount` DOUBLE NOT NULL,
+    `returned_amount` DOUBLE NOT NULL,
+
+    INDEX `Bills_bill_date_idx`(`bill_date`),
+    PRIMARY KEY (`bill_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BillStocks` (
+    `bill_id` VARCHAR(191) NOT NULL,
+    `stock_id` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL,
+
+    PRIMARY KEY (`bill_id`, `stock_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Returned_Items` (
+    `returned_id` VARCHAR(10) NOT NULL,
+    `returned_date` DATE NOT NULL,
+    `returned_quantity` INTEGER NOT NULL,
+    `reason` VARCHAR(255) NOT NULL,
+    `bill_id` VARCHAR(191) NOT NULL,
+    `stock_id` VARCHAR(191) NOT NULL,
+
+    INDEX `Returned_Items_bill_id_idx`(`bill_id`),
+    INDEX `Returned_Items_stock_id_idx`(`stock_id`),
+    PRIMARY KEY (`returned_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Unit_Conversions` ADD CONSTRAINT `Unit_Conversions_from_unit_fkey` FOREIGN KEY (`from_unit`) REFERENCES `Units`(`unit_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Unit_Conversions` ADD CONSTRAINT `Unit_Conversions_to_unit_fkey` FOREIGN KEY (`to_unit`) REFERENCES `Units`(`unit_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Items` ADD CONSTRAINT `Items_item_category_id_fkey` FOREIGN KEY (`item_category_id`) REFERENCES `Item_Categories`(`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ItemUnits` ADD CONSTRAINT `ItemUnits_item_id_fkey` FOREIGN KEY (`item_id`) REFERENCES `Items`(`item_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ItemUnits` ADD CONSTRAINT `ItemUnits_unit_id_fkey` FOREIGN KEY (`unit_id`) REFERENCES `Units`(`unit_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Stocks` ADD CONSTRAINT `Stocks_item_id_fkey` FOREIGN KEY (`item_id`) REFERENCES `Items`(`item_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Stocks` ADD CONSTRAINT `Stocks_supplier_id_fkey` FOREIGN KEY (`supplier_id`) REFERENCES `Suppliers`(`supplier_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BillStocks` ADD CONSTRAINT `BillStocks_bill_id_fkey` FOREIGN KEY (`bill_id`) REFERENCES `Bills`(`bill_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BillStocks` ADD CONSTRAINT `BillStocks_stock_id_fkey` FOREIGN KEY (`stock_id`) REFERENCES `Stocks`(`stock_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Returned_Items` ADD CONSTRAINT `Returned_Items_bill_id_fkey` FOREIGN KEY (`bill_id`) REFERENCES `Bills`(`bill_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Returned_Items` ADD CONSTRAINT `Returned_Items_stock_id_fkey` FOREIGN KEY (`stock_id`) REFERENCES `Stocks`(`stock_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
